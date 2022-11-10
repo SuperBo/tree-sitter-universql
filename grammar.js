2605cbbb663535a6c5dbcc914e09cd9075255760
Y Nguyen <y.nguyen@paymaya.com>
2022-11-10 14:10:40 +0700

    [feat] add zetasql parser.txt

--- grammar.js
+++ grammar.js
@@ -6,6 +6,7 @@ const KW = {
   DEFAULT: reserve('default'),
   DISTINCT: reserve('distinct'),
   DOCUMENT_P: reserve('document_p'),
+  EXPLAIN: reserve('explain'),
   FOR: reserve('for'),
   FROM: reserve('from'),
   GROUP: reserve('group'),
@@ -16,6 +17,7 @@ const KW = {
   ISNULL: reserve('isnull'),
   JOIN: reserve('join'),
   LIKE: reserve('like'),
+  LIMIT: reserve('limit'),
   NOT: reserve('not'),
   NOTNULL: reserve('notnull'),
   OR: reserve('or'),
@@ -24,8 +26,8 @@ const KW = {
   SYMMETRIC: reserve('symmetric'),
   TO: reserve('to'),
   USING: reserve('using'),
+  VERBOSE: reserve('verbose'),
   WHERE: reserve('where'),
-  LIMIT: reserve('limit'),
 };
 
 const PREC = {
@@ -69,7 +71,10 @@ module.exports = grammar({
   ],
 
   supertypes: $ => [
-    $.expr
+    $.expr,
+    $._stmt,
+    $._raw_type,
+    $.func_expr_common_subexpr,
   ],
 
   rules: {
@@ -81,7 +86,8 @@ module.exports = grammar({
       optional(';')
     ),
     _stmt: $ => choice(
-        $._select_stmt,
+      $._select_stmt,
+      $.explain,
     /*  $.alter_statement,
         $.analyze_statement,
         $.assert_statement,
@@ -132,7 +138,7 @@ module.exports = grammar({
         $.call_statement,
         $.import_statement,
         $.module_statement,
-*/
+        */
     ),
 
     /*==== Keywwords ====*/
@@ -142,12 +148,17 @@ module.exports = grammar({
       'aS',
       'As'
     )),
-    _kw_from: _ => token(prec(1, KW.FROM)),
+    _kw_and: _ => token(prec(1, KW.AND)),
+    _kw_or: _ => token(prec(1, KW.OR)),
     _kw_order: _ => token(prec(1, KW.ORDER)),
     _kw_limit: _ => token(prec(1, KW.LIMIT)),
+    _kw_analyze: _ => reserveMany('analyze', 'analyse'),
+    _kw_not: _ => token(prec(1, KW.NOT)),
+    _kw_like_ilike: _ => token(prec(1, reserveMany('like', 'ilike'))),
+    _kw_is: _ => token(prec(1, reserve('is'))),
 
     /*==== Literals ====*/
-    null: _ => reserve('null'),
+    null: _ => token(prec(1, reserve('null'))),
     unknow: _ => reserve('unknow'),
     boolean_literal: _ => reserveMany('true', "false"),
     
@@ -215,7 +226,7 @@ module.exports = grammar({
       ),
       token.immediate('"""')
     ),
-    escape_sequence: _ => token.immediate(/\\[rn\.]|\\r\\n/),
+    escape_sequence: _ => token(token.immediate(/\\(.|\n|\r|\r\n)/)),
 
     interval_literal: $ => choice(
       seq(KW.INTERVAL, $.string_literal, optional($.interval_end)),
@@ -249,22 +260,22 @@ module.exports = grammar({
 
     /* Type */
     // Unlike other type names, 'INTERVAL' is a reserved keyword.
-    type_name: $ => choice($.path_expr, KW.INTERVAL),
-    array_type: $ => seq(reserve('array'), '<', $.type, '>'),
+    _type_name: $ => choice($.path_expr, KW.INTERVAL),
+    array_type: $ => seq(token(prec(1, reserve('array'))), '<', $.type, '>'),
     struct_field: $ => seq(optional($.identifier), $.type),
-    struct_field_list: $ => seq($.struct_field, repeat(seq(',', $.struct_field))),
-    struct_type: $ => seq(reserve('struct'), '<', optional($.struct_field_list), '>'), 
-    raw_type: $ => choice($.array_type, $.struct_type, $.type_name),
-    type_param_field: $ => choice(
+    //struct_field_list: $ => commaSep1($.struct_field),
+    struct_type: $ => seq(token(prec(1, reserve('struct'))), '<', optional(commaSep1($.struct_field)), '>'), 
+    _raw_type: $ => choice($.array_type, $.struct_type, $._type_name),
+    _type_param_field: $ => choice(
       $.integer_literal,
       $.boolean_literal,
       $.string_literal,
       $.bytes_literal,
       $.float_literal,
-      'MAX'
+      reserve('max'),
     ),
-    type_param: $ => seq('(', $.type_param_field, repeat(seq(',', $.type_param_field)), ')'),
-    type: $ => seq($.raw_type, optional($.type_param), optional($.collate_clause)),
+    type_param: $ => seq('(', $._type_param_field, repeat(seq(',', $._type_param_field)), ')'),
+    type: $ => seq($._raw_type, optional($.type_param), optional($.collate_clause)),
 
     /*==== Clause ====*/
     _string_lit_or_param: $ => choice(
@@ -288,20 +299,20 @@ module.exports = grammar({
       // prec(PREC.COMPARE, seq($._expr_primary, optNot(), KW.IN, $.in_expr)),
       // DEFAULT
     ),
-    logical_unary_expr: $ => prec(PREC.LOGICAL_NOT, seq(KW.NOT, $.expr)),
+    logical_unary_expr: $ => prec(PREC.LOGICAL_NOT, seq($._kw_not, $.expr)),
     logical_binary_expr: $ => choice(
-      prec.left(PREC.LOGICAL_AND, seq($.expr, KW.AND, $.expr)),
-      prec.left(PREC.LOGICAL_OR, seq($.expr, KW.OR, $.expr)),
+      prec.left(PREC.LOGICAL_AND, seq($.expr, $._kw_and, $.expr)),
+      prec.left(PREC.LOGICAL_OR, seq($.expr, $._kw_or, $.expr)),
     ),
     compare_expr: $ => choice(
-      prec(PREC.COMPARE, seq($._expr_primary, optNot(), choice(KW.LIKE, KW.ILIKE), $._expr_primary)),
+      prec(PREC.COMPARE, seq($._expr_primary, optNot(), $._kw_like_ilike, $._expr_primary)),
       prec(PREC.COMPARE, seq($._expr_primary, optNot(), reserve('similar'), KW.TO, $._expr_primary)),
-      prec(PREC.COMPARE, seq($._expr_primary, KW.IS, optional(KW.NOT),
+      prec(PREC.COMPARE, seq($._expr_primary, $._kw_is, optional(KW.NOT),
         choice($.null, $.boolean_literal, $.unknow))),
       prec(PREC.COMPARE, seq($._expr_primary, choice(KW.NOTNULL, KW.ISNULL))),
       prec(PREC.COMPARE, seq($._expr_primary, optional(KW.ASYMETRIC), KW.BETWEEN, $._expr_primary, KW.AND, $._expr_primary)),
       prec(PREC.COMPARE, seq($._expr_primary, optNot(), KW.BETWEEN, KW.SYMMETRIC, $._expr_primary, KW.AND, $._expr_primary)),
-      prec(PREC.COMPARE, seq($._expr_primary, KW.IS, optNot(), optional($.unicode_normal_form), reserve('normalized'))),
+      prec(PREC.COMPARE, seq($._expr_primary, $._kw_is, optNot(), optional($.unicode_normal_form), reserve('normalized'))),
     ),
     _expr_primary: $ => choice(
       $._c_expr,
@@ -311,7 +322,7 @@ module.exports = grammar({
       $.binary_expr,
     ),
     at_expr: $ => prec.left(PREC.AT, seq($._expr_primary, $._at_time_zone)),
-    _at_time_zone: $ => seq(reserve('at'), reserve('time'), reserve('zone'), $._const_expr),
+    _at_time_zone: $ => seq(reserve('at'), reserve('time'), reserve('zone'), $._expr_primary),
     pg_cast_expr:  $ => prec(PREC.CAST, seq($._expr_primary, '::', $.type)),// Postgres cast "::"
     unary_expr: $ => prec(PREC.UMINUS, seq(/~|\+|-/, $._expr_primary)),
     binary_expr: $ => choice(
@@ -335,10 +346,13 @@ module.exports = grammar({
      */
     _c_expr: $ => choice(
       $.path_expr,
+      $.dot_identifier,
+      //$.dot_field,
       $._const_expr,
       $.paren_expr,
       $.case_expr,
       $.func_expr,
+      $.array_expr
     ),
     paren_expr: $ => seq('(', $.expr ,')'),
 
@@ -355,22 +369,32 @@ module.exports = grammar({
       $.date_or_time_literal
     ),
     //TODO: add /* generic type 'literal' syntax */, and /* generic syntax with a type modifier */
+    _numeric_only: $ => choice(
+      $.float_literal,
+      seq(/\+|-/, $.float_literal),
+      $.integer_literal,
+      seq(/\+|-/, $.interval_literal)
+    ),
+    dot_identifier: $ => prec(PREC.PRIMARY, seq($._expr_primary, '.', $.identifier)),
+    dot_field: $ => prec(PREC.PRIMARY, seq($.expr, '.', '(', $.path_expr, ')')),
 
     /* Function Expression */
     func_expr: $ => choice(
       seq($.func_app, optional($.within_group_clause), optional($.filter_clause), optional($._over_clause)),
       $.func_expr_common_subexpr
     ),
-    func_app: $ => seq($.path_expr, '(', optional(choice(
-      $.star,
+    func_app: $ => seq(field('name', $.path_expr), '(', optional(choice(
       seq(alias($.func_arg_list_variadic, $.func_arg_list), optOrderBy($)),
       seq($.func_arg_variadic, optOrderBy($)),
       seq($.set_quantifier, $.func_arg_list, optOrderBy($)),
     )), ')'),
     func_arg_variadic: $ => seq(reserve('variadic'), $._func_arg_expr),
+    // The first argument may be a "*" instead of an expression
+    // This is valid for COUNT(*), which has no other arguments
+    // and ANON_COUNT(*), which has multiple other arguments.
     func_arg_list: $ => commaSep1($._func_arg_expr),
     func_arg_list_variadic: $ => seq(
-      $._func_arg_expr, repeat(seq(',', $._func_arg_expr)), optional(seq(',', $.func_arg_variadic))
+      choice($._func_arg_expr, $.star), repeat(seq(',', $._func_arg_expr)), optional(seq(',', $.func_arg_variadic))
     ),
     _func_arg_expr: $ => choice(
       $.expr,
@@ -380,25 +404,17 @@ module.exports = grammar({
 
     /* Special expressions that are considered to be functions.*/
     func_expr_common_subexpr: $ => choice(
-      seq(reserve('collation'), KW.FOR, '(', $.expr, ')'),
-       reserveMany(
-        'current_date',
-        'current_role', 'current_user', 'session_user', 'system_user', 'user',
-        'current_catalog', 'current_schema'
-      ),
-      seq(
-        reserveMany('current_time', 'current_timestamp', 'localtime', 'localtimestamp'),
-        optional(seq('(', $.integer_literal, ')'))
-      ),
+      $.parenless_func_expr,
+      $.collation_expr,
+      $.current_time_expr,
       $.cast_expr,
       $.extract_expr,
       $.substr_expr,
-      seq(reserve('normalize'), '(', $.expr, optional(seq(',', $.unicode_normal_form)),')'),
-      seq(reserve('overlay'), '(', $.overlay_list, ')'),
-      seq(reserve('overlay'), '(', optional($.func_arg_list), ')'),
-      seq(reserve('position'), '(', $._expr_primary, KW.IN, $._expr_primary, ')'),
-      seq(reserve('treat'), '(', $.expr, $._kw_as, $.type, ')'),
-      seq(reserve('trim'), '(', optional(reserveMany('both', 'leading', 'trailing')), $.trim_list, ')'),
+      $.trim_expr,
+      $.normalize_expr,
+      $.overlay_expr,
+      $.position_expr,
+      $.treat_expr,
       $.xml_expr,
     ),
 
@@ -434,16 +450,14 @@ module.exports = grammar({
       reserve('end')
     ),
     unicode_normal_form: _ => reserveMany('nfc', 'nfd', 'nfkc', 'nfkd'), 
-    path_expr: $ => seq(
-      $.identifier,
-      repeat(seq('.', $.identifier))
-    ),
+    path_expr: $ => prec.right(seq($.identifier, repeat(seq('.', $.identifier)))),
     unnest_expr: $ => seq(reserve('unnest'), '(', $.expr, ')'),
     // convert_time_zone: $ => prec.left(, seq($.expr, $._at_time_zone)),
     // _at_time_zone: $ => seq(reserve('at'), reserve('time'), reserve('zone'), $._expr_primary),
-    _format: $ => seq(reserve('format'), $.expr, optional($._at_time_zone)),
+    _format: $ => seq(reserve('format'), $.expr),
     cast_expr: $ => seq(
-      reserveMany('cast', 'safe_cast'), '(', $.expr, $._kw_as, $.type, optional($._format), ')'
+      token(prec(1, reserveMany('cast', 'safe_cast'))),
+      '(', $.expr, $._kw_as, $.type, optional(field('format', $._format)), ')'
     ),
     extract_expr: $ => seq(
       reserve('extract'), '(', $.expr, KW.FROM, $.expr, optional($._at_time_zone), ')'
@@ -453,14 +467,29 @@ module.exports = grammar({
       seq($.expr, KW.FOR, $.expr, KW.FROM,  $.expr),
       seq($.expr, reserve('similar'), $.expr, reserve('escape'), $.expr)
     ),
+    substr_expr: $ => seq(reserve('substring'), '(', choice(
+      $.substr_list, optional($.func_arg_list)
+    ), ')'),
     trim_list: $ => choice(
       seq($.expr, KW.FROM, $.expr_list),
       seq(KW.FROM, $.expr_list),
       $.expr_list
     ),
-    substr_expr: $ => seq(reserve('substring'), '(', choice(
-      $.substr_list, optional($.func_arg_list)
-    ), ')'),
+    trim_expr: $ => seq(reserve('trim'), '(', optional(reserveMany('both', 'leading', 'trailing')), $.trim_list, ')'),
+    normalize_expr: $ => seq(reserve('normalize'), '(', $.expr, optional(seq(',', $.unicode_normal_form)),')'),
+    overlay_expr: $ => seq(reserve('overlay'), '(', optional(choice($.overlay_list, $.func_arg_list)), ')'),
+    position_expr: $ => seq(reserve('position'), '(', $._expr_primary, KW.IN, $._expr_primary, ')'),
+    treat_expr: $ => seq(reserve('treat'), '(', $.expr, $._kw_as, $.type, ')'),
+    current_time_expr: $ =>  seq(
+      reserveMany('current_time', 'current_timestamp', 'localtime', 'localtimestamp'),
+      optional(seq('(', $.integer_literal, ')'))
+    ),
+    collation_expr: $ => seq(reserve('collation'), KW.FOR, '(', $.expr, ')'),
+    parenless_func_expr: _ => reserveMany(
+      'current_date',
+      'current_role', 'current_user', 'session_user', 'system_user', 'user',
+      'current_catalog', 'current_schema'
+    ),
     // Postgres XML function
     xml_expr: $ => choice(
       seq(reserve('xmlconcat'), '(', $.expr_list, ')'),
@@ -518,7 +547,7 @@ module.exports = grammar({
     )),
     xmltable_column_list: $ => commaSep1($.xmltable_column_element),
     xmltable_column_element: $ => choice(
-      seq($.identifier, $.type_name, $.xmltable_column_option_list),
+      seq($.identifier, $._type_name, $.xmltable_column_option_list),
       seq($.identifier, KW.FOR, reserve('ordinality')),
     ),
     xmltable_column_option_list: $ => repeat1($.xmltable_column_option_element),
@@ -566,13 +595,13 @@ module.exports = grammar({
       seq(reserve('no'), reserve('others'))
     )),
     expr_list: $ => seq($.expr, repeat(seq(',', $.expr))),
-    /*
+
     _array_construct_prefix: $ => choice(
       reserve('array'),
       $.array_type
     ),
-    array_constructor: $ => seq(optional($._array_construct_prefix), '[', optional($._expr_list), ']'),
-
+    array_expr: $ => seq(optional($._array_construct_prefix), '[', optional($.expr_list), ']'),
+    /*
     _format: $ => seq(reserve('format'), $.expression, optional($._at_time_zone)),
     cast_expr: $ => seq(
       reserve_many('cast', 'safe_cast'), '(', $.expression, reserve('as'), $.type, optional($._format), ')'
@@ -624,7 +653,7 @@ module.exports = grammar({
     
     // select_spec ~ 7.16 <query specification> in foundation grammar
     select_spec: $ => seq(
-      reserve('select'), optional($.set_quantifier),
+      token(prec(1, reserve('select'))), optional($.set_quantifier),
       field('select_list', $.select_list),
       field('into', optional($.into_clause)),
       field('from', optional($._from_clause)),
@@ -656,14 +685,14 @@ module.exports = grammar({
 
     // Specify a table derived from one or more tables.
     // ~ <from clause> in foundation grammar
-    _from_clause: $ => seq($._kw_from, $.from_list),
+    _from_clause: $ => seq(token(prec(1, KW.FROM)), $.from_list),
     from_list: $ => commaSep1($.table_ref),
     // Reference a table ~ <table reference> in foundation grammar.
     table_ref: $ =>  choice(
       seq($._relation_expr, optional($._alias_clause), field('sample', optional($.sample_clause))),
       seq(reserveOpt('lateral'), $.func_table, optional($.func_alias_clause)),
       seq(reserveOpt('lateral'), $.xmltable, optional($._alias_clause)),
-      seq(reserve('lateral'), $.select_in_parens, optional($._alias_clause)),
+      seq(reserveOpt('lateral'), alias($.select_in_parens, $.subquery_table), optional($._alias_clause)),
       $.joined_table,
       seq('(', $.joined_table , ')', $._alias_clause),
     ),
@@ -790,7 +819,7 @@ module.exports = grammar({
     join_hint: reserveMany('hash', 'lookup'), */
 
     /* Where clause in select */
-    _where_clause: $ => seq(reserve('where'), $.expr),
+    _where_clause: $ => seq(token(prec(1, reserve('where'))), $.expr),
     /* Group by and having clause */
     _group_by_clause: $ => seq(KW.GROUP, KW.BY, optional($.set_quantifier), $.group_by_list),
     group_by_list: $ => commaSep1($._group_by_item),
@@ -857,6 +886,27 @@ module.exports = grammar({
         seq(reserve('to'), $._const_expr, KW.DEFAULT, $._const_expr, KW.USING, $.identifier)
       ))
     ), // TODO: change to column_list and column_id later
+    
+    /*****************************************************************************
+     *
+     *		QUERY:
+     *				EXPLAIN [ANALYZE] [VERBOSE] query
+     *				EXPLAIN ( options ) query
+     *****************************************************************************/
+    _explainable_stmt: $ => choice(
+      $._select_stmt,
+      //TODO: add other statements
+    ),
+    utility_option_list: $ => commaSep1($.utility_option_item),
+    utility_option_item: $ => seq($.utility_option_name, optional($.utility_option_arg)),
+    utility_option_name: $ => choice($._kw_analyze, $.identifier), //TODO: change identifier to NonReservedWord
+    utility_option_arg: $ => choice($.boolean_literal, $.string_literal, $._numeric_only),
+    explain: $ => seq(KW.EXPLAIN, choice(
+      $._explainable_stmt,
+      seq($._kw_analyze, optional(KW.VERBOSE), $._explainable_stmt),
+      seq(KW.VERBOSE, $._explainable_stmt),
+      seq('(', $.utility_option_list, ')', $._explainable_stmt)
+    )),
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     comment: $ => token(choice(
